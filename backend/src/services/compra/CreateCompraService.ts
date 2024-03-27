05a2bdb28aa96e3a08e52b52d6148fa75706703a
Elton Macuacua <97108612+eltonmacuacua@users.noreply.github.com>
2024-03-27 16:16:56 +0200

    compra de produto
    adicao de exceptions

--- backend/src/services/compra/CreateCompraService.ts
+++ backend/src/services/compra/CreateCompraService.ts
@@ -1,15 +1,27 @@
 import prismaClient from "../../prisma";
 
-
 interface CompraRequest {
     dataCompra: Date;
     id_cliente: string;
     id_produto: string;
-    quantidade: number; 
+    quantidade: number;
 }
 
 class CreateCompraService {
     async handle({ dataCompra, id_cliente, id_produto, quantidade }: CompraRequest) {
+        const produto = await prismaClient.produtos.findUnique({
+            where: { id: id_produto },
+            select: { quantidadeDisponivel: true }
+        });
+
+        if (!produto) {
+            throw new Error("Produto não encontrado");
+        }
+
+        if (quantidade > produto.quantidadeDisponivel) {
+            throw new Error("Quantidade comprada excede a quantidade disponível");
+        }
+
         const transaction = await prismaClient.$transaction([
             prismaClient.compra.create({
                 data: {
@@ -22,13 +34,13 @@ class CreateCompraService {
                 where: { id: id_produto },
                 data: {
                     quantidadeDisponivel: {
-                        decrement: quantidade 
+                        decrement: quantidade
                     }
                 }
             })
         ]);
 
-        return transaction[0]; 
+        return transaction[0];
     }
 }
 
